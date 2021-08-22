1c69cf5662a9395eabb2fcb942d7d501bf6a0250
lhernandeza <lhernandez@hmd.consulting>
2021-08-21 21:42:56 -0600

    first commit

---
+++ hola_mundo/src/hola_mundo/Main.java
@@ -0,0 +1,9 @@
+package hola_mundo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Hola Mundo");
+	}
+
+}
---
+++ tipos_datos/src/tipos_datos/Main.java
@@ -0,0 +1,13 @@
+package tipos_datos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int entero=1;
+		double doble=2.0;
+		float flotante =2.3f;
+		
+
+	}
+
+}
